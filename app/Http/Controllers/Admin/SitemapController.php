f57b6a25f01e2482748788df1a9bb02288e5d7bb
Ibrahim Sadour <i.m.s.1995@hotmail.com>
2023-02-24 20:28:26 +0100

    fix seo speed

--- app/Http/Controllers/Admin/SitemapController.php
+++ app/Http/Controllers/Admin/SitemapController.php
@@ -57,7 +57,7 @@ class SitemapController extends Controller
             }
 
         }
-        return  $sitemap->writeToFile(public_path('sitemap_city_tags.xml'));
+        return  $sitemap->writeToFile(public_path('sitemap-city-tags.xml'));
     }
     public function sitemap_tags(){
         $tags = Tag::Active()->get();
